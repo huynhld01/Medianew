5f0c5924bde5ddd4f157a2b6971f8dfd88651a38
Huynhdl <53102588+huynhld01@users.noreply.github.com>
2020-11-13 09:29:56 +0700

    Update 100_in_chanle.c

--- 100_in_chanle.c
+++ 100_in_chanle.c
@@ -1,34 +1,58 @@
-#include <stdio.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
 
-int main()
+#define DEVICE_AUTHOR   "huynhLD"
+#define DEVICE_DESC     "Just example"
+ 
+dev_t dev = 0;
+static struct class *my_dev_class;
+ 
+/* module init function*/
+static int __init hello_world_init(void)
 {
-    int n; scanf("%d", &n);
-    int a[n], Ch[n], Le[n];
-    int l=0, c=0, dem=0;
-    for (int i=0; i<n; ++i){
-        scanf("%d", &a[i]);
+    if ((alloc_chrdev_region(&dev, 0, 1, "HuynhLD")) < 0)
+    {
+	printk(KERN_INFO "Cannot allocate major number for device\n");
+	return -1;
+    }
+    printk(KERN_INFO "Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+    /*Creating struct class*/
+    if((my_dev_class = class_create(THIS_MODULE,"HuynhLD")) == NULL)
+    {
+        printk(KERN_INFO "Cannot create the struct class for device\n");
+        unregister_chrdev_region(dev,1);
+    }
+ 
+    /*Creating device*/
+    if((device_create(my_dev_class,NULL,dev,NULL,"HuynhLD")) == NULL)
+    {
+        printk(KERN_INFO "Cannot create the Device\n");
+        class_destroy(my_dev_class);
     }
 
-    for (int i=0; i<n; ++i)
-    {
-        if (a[i] % 2==0) {
-            Ch[c++]=a[i];
-            dem++;
-        }
-        else {
-            Le[l++]=a[i];
-        }
-    }
-     printf("%d ", n-dem);
-    for (int i = 0; i < n-dem; i++)
-    {
-        printf("%d ", Le[i]);
-    }
-   
-     printf("\n");
-   printf("%d ", dem);
-    for (int i = 0; i < dem; i++)
-    {
-        printf("%d ", Ch[i]);
-    }
+    printk(KERN_INFO "Kernel Module Inserted Successfully...\n");
+    return 0;
 }
+
+ 
+/* module exit function */
+static void __exit hello_world_exit(void)
+{
+    unregister_chrdev_region(dev, 1);
+    device_destroy(my_dev_class,dev);
+    class_destroy(my_dev_class);
+    printk(KERN_INFO "Kernel Module Removed Successfully...\n");
+}
+ 
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("DEVICE_AUTHOR");
+MODULE_DESCRIPTION("DEVICE_DESC");
+MODULE_VERSION("1.0");
